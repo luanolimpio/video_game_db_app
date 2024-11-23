809575c69a494f495f935913cf4fb6e23147c7cc
Luan Olimpio <luanmaia05@hotmail.com>
2024-11-23 16:50:41 -0300

    feat: add splash screen

--- app/src/main/java/com/example/videogamedbapp/MainActivity.kt
+++ app/src/main/java/com/example/videogamedbapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.videogamedbapp.presentation.navigation.MainNavigator
 import com.example.videogamedbapp.ui.theme.VideoGameDBAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             VideoGameDBAppTheme {
