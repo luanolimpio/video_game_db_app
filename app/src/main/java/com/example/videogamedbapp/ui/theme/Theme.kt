d6d2a748a5e66b53332bb5bdc7da4962352d52f6
Luan Olimpio <luanmaia05@hotmail.com>
2024-11-23 18:21:14 -0300

    feat: add surface variant on Theme

--- app/src/main/java/com/example/videogamedbapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/videogamedbapp/ui/theme/Theme.kt
@@ -1,41 +1,31 @@
 package com.example.videogamedbapp.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Green80,
     onPrimary = Green20,
     primaryContainer = Green30,
     onPrimaryContainer = Green90,
-//    inversePrimary = Green40,
     secondary = DarkGreen80,
     onSecondary = DarkGreen20,
     secondaryContainer = DarkGreen30,
     onSecondaryContainer = DarkGreen90,
-//    tertiary = Violet80,
-//    onTertiary = Violet20,
-//    tertiaryContainer = Violet30,
-//    onTertiaryContainer = Violet90,
-//    error = Red80,
-//    onError = Red20,
-//    errorContainer = Red30,
-//    onErrorContainer = Red90,
     background = Grey10,
     onBackground = Grey90,
     surface = GreenGrey30,
     onSurface = GreenGrey80,
-
-
-//    inverseSurface = Grey90,
-//    inverseOnSurface = Grey10,
-//    surfaceVariant = GreenGrey30,
-//    onSurfaceVariant = GreenGrey80,   //talvez
-//    outline = GreenGrey80
+    surfaceVariant = GreenGrey30,
+    onSurfaceVariant = GreenGrey80,
 )
 
 private val LightColorScheme = lightColorScheme(
@@ -43,67 +33,17 @@ private val LightColorScheme = lightColorScheme(
     onPrimary = Color.White,
     primaryContainer = Green90,
     onPrimaryContainer = Green10,
-//    inversePrimary = Green80,
     secondary = DarkGreen40,
     onSecondary = Color.White,
     secondaryContainer = DarkGreen90,
     onSecondaryContainer = DarkGreen10,
-//    tertiary = Violet40,
-//    onTertiary = Color.White,
-//    tertiaryContainer = Violet90,
-//    onTertiaryContainer = Violet10,
-//    error = Red40,
-//    onError = Color.White,
-//    errorContainer = Red90,
-//    onErrorContainer = Red10,
     background = Grey99,
     onBackground = Grey10,
     surface = GreenGrey90,
     onSurface = GreenGrey30,
-
-
-
-//    inverseSurface = Grey20,
-//    inverseOnSurface = Grey95,
-//    surfaceVariant = GreenGrey90,
-//    onSurfaceVariant = GreenGrey30,  //talvez
-//    outline = GreenGrey50
-
-
-
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    surfaceVariant = GreenGrey90,
+    onSurfaceVariant = GreenGrey30,
 )
-
-//private val DarkColorScheme = darkColorScheme(
-//    primary = Purple80,
-//    secondary = PurpleGrey80,
-//    tertiary = Pink80
-//)
-//
-//private val LightColorScheme = lightColorScheme(
-//    primary = Purple40,
-//    secondary = PurpleGrey40,
-//    tertiary = Pink40
-//
-//    /* Other default colors to override
-//    background = Color(0xFFFFFBFE),
-//    surface = Color(0xFFFFFBFE),
-//    onPrimary = Color.White,
-//    onSecondary = Color.White,
-//    onTertiary = Color.White,
-//    onBackground = Color(0xFF1C1B1F),
-//    onSurface = Color(0xFF1C1B1F),
-//    */
-//)
 
 @Composable
 fun VideoGameDBAppTheme(
@@ -113,10 +53,10 @@ fun VideoGameDBAppTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-//      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-//        val context = LocalContext.current
-//        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-//      }
+      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        val context = LocalContext.current
+        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+      }
       darkTheme -> DarkColorScheme
       else -> LightColorScheme
     }
